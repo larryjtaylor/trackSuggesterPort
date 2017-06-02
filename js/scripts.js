c926bf925935e6dd786b189cf39b7311239ffd2e
Larry Taylor <larryjamestaylor@gmail.com>
2017-06-02 13:27:32 -0700

    try options for displaying results based on select inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
-  $(".question-box").submit(function() {
-    var co_size = parseint($("input#co_size").val());
-    var platform = parseint($("input#platform").val());
-    var stack = parseint($("input#stack").val());
-    var city = parseint($("input#city").val());
+  $("select.question-box").submit(function() {
+    var co_size = document.getElementById("#co_size");
+      var selection = co_size.options[co_size.selectedIndex].value;
+    var platform = document.getElementById("select#platform");
+    var stack = document.getElementById("select#stack");
+    var city = document.getElementById("select#city");
+
+    if(selection === "large") {
+      alert("it works");
+      // $("#temp").hide();
+      // $("#ruby").show();
+    } else if () {
+      $("#temp").hide();
+      $("#java").show();
+    } else () {
+      $("#temp").hide();
+      $("#css").show();
+    }
   });
 });
