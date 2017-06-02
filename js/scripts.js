e5435a182e382b7fbe0934a3607d0c029fb702b8
Larry Taylor <larryjamestaylor@gmail.com>
2017-06-02 12:22:22 -0700

    define variables in js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".question-box").submit(function() {
+    var co_size = parseint($("input#co_size").val());
+    var platform = parseint($("input#platform").val());
+    var stack = parseint($("input#stack").val());
+    var city = parseint($("input#city").val());
+  });
+});
